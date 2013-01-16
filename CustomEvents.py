282103649fcd69e75f96a4e53819aa28dee6fe47
anhoppe <anhoppe@64415236-580a-e06f-fc55-ebd5ab250b58>
2013-01-16 16:39:19 +0000

    added to repo

---
+++ CustomEvents.py
@@ -0,0 +1,13 @@
+import wx.lib.newevent
+
+PointAddEvent, EVT_POINT_ADD = wx.lib.newevent.NewEvent()
+
+PolygonNewEvent, EVT_POLYGON_NEW = wx.lib.newevent.NewEvent()
+PolygonClearEvent, EVT_POLYGON_CLEAR = wx.lib.newevent.NewEvent()
+PolygonClearAllEvent, EVT_POLYGON_CLEAR_ALL = wx.lib.newevent.NewEvent()
+PolygonNextEvent, EVT_POLYGON_NEXT = wx.lib.newevent.NewEvent()
+PolygonPrevEvent, EVT_POLYGON_PREV = wx.lib.newevent.NewEvent()
+PolygonRefreshEvent, EVT_POLYGON_REFRESH = wx.lib.newevent.NewEvent()
+PolygonResetEvent, EVT_POLYGON_RESET = wx.lib.newevent.NewEvent()
+RasterResizeEvent, EVT_RASTER_RESIZE = wx.lib.newevent.NewEvent()
+RasterPositionEvent, EVT_RASTER_POSITION = wx.lib.newevent.NewEvent()
